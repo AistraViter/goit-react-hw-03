ac3502ea4497e7ba7ed66a132423ea571c922091
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-07-20 19:14:48 +0300

    Step3 Formic props InitialValues added

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,37 @@
 import { useId } from "react";
+import { Formik, Form, Field } from "formik";
 import styles from "./ContactForm.module.css";
 
 const ContactForm = ({ handleSubmit }) => {
   const { contactForm } = styles;
   const id = useId();
+  const initialValues = {
+    name: "",
+    number: "",
+  };
 
   return (
-    <form onSubmit={handleSubmit} className={contactForm}>
-      <div>
-        <label htmlFor={`contactFormName${id}`}>Name</label>
-        <input id={`contactFormName${id}`} type="text" name="name" required />
-      </div>
-      <div>
-        <label htmlFor={`contactFormNumber${id}`}>Number</label>
-        <input
-          id={`contactFormNumber${id}`}
-          type="tel"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-          placeholder="000-000-0000" /* Додаємо зразок формату у плейсхолдер */
-          name="number"
-          required
-        />
-      </div>
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      <Form onSubmit={handleSubmit} className={contactForm}>
+        <div>
+          <label htmlFor={`contactFormName${id}`}>Name</label>
+          <Field id={`contactFormName${id}`} type="text" name="name" required />
+        </div>
+        <div>
+          <label htmlFor={`contactFormNumber${id}`}>Number</label>
+          <Field
+            id={`contactFormNumber${id}`}
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}"
+            placeholder="000-000-00" /* Додаємо зразок формату у плейсхолдер */
+            name="number"
+            required
+          />
+        </div>
 
-      <button type="submit">Add contact</button>
-    </form>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
   );
 };
 export default ContactForm;
