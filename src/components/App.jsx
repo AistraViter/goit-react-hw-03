e0b05646234ea0ee4ed34592b161bb5386e27cda
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-07-21 02:14:27 +0300

    Step 5 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
 import styles from "./App.module.css";
 
-const contacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+const savedContacts = JSON.parse(localStorage.getItem("saved-contacts"));
+
+const contacts =
+  savedContacts !== null
+    ? savedContacts
+    : [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ];
 
 function App() {
   const { container, phonebookTitle } = styles;
@@ -25,8 +30,11 @@ function App() {
     };
     setContacts((prevContacts) => [...prevContacts, newContact]);
     actions.resetForm();
-
   };
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(newContacts));
+  }, [newContacts]);
 
   const deleteContact = (id) => {
     setContacts((prevContacts) =>
